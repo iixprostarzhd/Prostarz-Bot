4549505e98dbede1162ef7de852a498f9a3f5a27
iixprostarzhd <id545262@live.com>
2018-03-20 10:31:17 +0000

    fixed guild

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ module.exports.play = function play(guild, song) {
       if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
       console.log(reason);
       serverQueue.songs.shift();
-      module.exports.play(guild, serverQueue.songs[0]);
+      module.exports.play(message.guild, serverQueue.songs[0]);
     })
     .on("error", error => console.error(error));
 
