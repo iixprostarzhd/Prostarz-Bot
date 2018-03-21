89f2e12b263d49f2bae03989bdcd5502b6697c36
iixprostarzhd <id545262@live.com>
2018-03-21 02:16:53 +0000

    fixed some stuff

--- commands/np.js
+++ commands/np.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
 
   if (!serverQueue) return message.channel.send("There is nothing playing");
 
-  const videoID = youtube.getVideoById(`https://www.youtube.com/watch?v=${serverQueue.songs[0].id}`);
+  const videoID = youtube.getVideoByID(serverQueue.songs[0].id);
   console.log(videoID);
   return message.channel.send(`Now playing: **${serverQueue.songs[0].title}**`);
 }
