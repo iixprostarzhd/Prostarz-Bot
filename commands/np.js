ad67cdcee59f80b563b7b1228e19abf54c51c627
iixprostarzhd <id545262@live.com>
2018-03-21 03:42:00 +0000

    added some stuff

--- commands/np.js
+++ commands/np.js
@@ -15,6 +15,7 @@ module.exports.run = async (bot, message, args) => {
   if (!serverQueue) return message.channel.send("There is nothing playing");
 
   const videoID = await youtube.getVideoByID(serverQueue.songs[0].id);
+  console.log(videoID);
 
   let thumbNail = videoID.thumbnails.maxres.url;
   if (!videoID.thumbnails.maxres.url) {
