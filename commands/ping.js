b883d6f5c5a3b991f745aee49c8676ba5df86465
iixprostarzhd <id545262@live.com>
2018-04-04 18:12:51 +0100

    fixed ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  const msg = await message.channel.send("Ping?");
+  const m = await message.channel.send("Ping?");
 
-  msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 }
 
 module.exports.help = {
